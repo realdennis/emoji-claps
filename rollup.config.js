8fc19c32c445fce36e27f4955e59551f31039a4e
realdennis <realdennis777@gmail.com>
2019-12-15 22:44:50 +0800

    Using rollup and zwitterion

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+export default [
+  {
+    input: "src/emoji-claps.ts",
+    output: {
+      file: "dist/emoji-claps.js",
+      format: "esm"
+    },
+    plugins: [typescript()],
+    external: [
+      "lit-element",
+      "@realdennis/next-frame",
+      "lodash-es",
+      "suicide-animate",
+      "element-animate-throttle"
+    ]
+  },
+  {
+    input: "src/emoji-claps.ts",
+    output: {
+      file: "dist/emoji-claps.umd.js",
+      format: "umd",
+      name: "emoji-claps"
+    },
+    plugins: [resolve(), typescript(), terser()]
+  }
+];
